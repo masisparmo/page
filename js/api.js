d828101181666876b1b81678ede49b754d63903b
ISPARMO <isparmo@hotmail.com>
2025-12-15 15:49:30 +0700

    Update GAS_URL to new endpoint

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@ import { MOCK_DATA } from './mock-data.js';
 
 // --- CONFIGURATION ---
 const USE_MOCK = false; // Set FALSE jika sudah deploy GAS
-const GAS_URL = "https://script.google.com/macros/s/AKfycbydn7E1gtSdi_3AhuSOMLTsUIOW-jk4_zYlWCZ5g6ETmh2V4FE-wk2TWUBgrLX1pvD8CA/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzQZd2WX8RkJix13CxjwEmHAMG0iQNAFqilNJi7UY4dddR4yYq-k7YQcyar6Be7fGxhzA/exec";
 
 // --- STATE MANAGEMENT ---
 let localData = null; // Menyimpan data yang di-fetch agar tidak request berulang kali jika tidak perlu
